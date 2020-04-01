8fedf7efb7e21a699f8195de473fb90525e32a33
niteshrupesh4 <niteshrupesh4@gmail.com>
2020-04-01 17:12:03 +0530

    Initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { Routes } from "@angular/router";
+import { FullComponent } from "./layouts/full/full.component";
+
+export const AppRoutes: Routes = [
+  {
+    path: "",
+    component: FullComponent,
+    children: [
+      {
+        path: "",
+        redirectTo: "/dashboard",
+        pathMatch: "full"
+      },
+      {
+        path: "",
+        loadChildren: () =>
+          import("./material-component/material-component.module").then( m => m.MaterialComponentModule)
+      },
+      {
+        path: "dashboard",
+        loadChildren: () =>
+          import("./dashboard/dashboard.module").then(m => m.DashboardModule)
+      }
+    ]
+  }
+];
