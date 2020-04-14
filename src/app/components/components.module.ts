0733cdb8e6a3741cbde20bde0be89cb3ee6b08bf
niteshrupesh4 <niteshrupesh4@gmail.com>
2020-04-14 14:10:56 +0530

    4 files modified

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,16 +3,18 @@ import { CommonModule } from '@angular/common';
 import { FormsComponent } from './forms/forms.component';
 import { ComponentsRoutingModule } from './components-routing.module';
 import { MaterialModule } from '../material/material.module';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { VideoPlayerComponent } from './video-player/video-player.component';
 import { MultiselectDropdownComponent } from './multiselect-dropdown/multiselect-dropdown.component';
-
+import { AngularMultiSelectModule } from 'angular2-multiselect-dropdown';
 @NgModule({
   imports: [
     CommonModule,
     ComponentsRoutingModule,
     MaterialModule,
-    FormsModule
+    FormsModule,
+    AngularMultiSelectModule,
+    ReactiveFormsModule
   ],
   declarations: [FormsComponent, VideoPlayerComponent, MultiselectDropdownComponent]
 })
