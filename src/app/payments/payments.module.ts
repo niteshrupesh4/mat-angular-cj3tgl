ea5aef12893117ca8475976d512b642cbe10624e
niteshrupesh4 <niteshrupesh4@gmail.com>
2020-04-23 11:56:57 +0530

    1 file modified

--- src/app/payments/payments.module.ts
+++ src/app/payments/payments.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PaymentsRoutingModule } from './payemnts-routing.module';
+import { MaterialModule } from '../material/material.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
-    PaymentsRoutingModule
+    PaymentsRoutingModule,
+    FormsModule,
+    MaterialModule
   ],
   declarations: []
 })
