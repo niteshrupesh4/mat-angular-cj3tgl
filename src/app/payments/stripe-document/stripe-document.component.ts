a41060fc437f43a7b7225d08edb16c075fd20850
niteshrupesh4 <niteshrupesh4@gmail.com>
2020-05-05 18:13:16 +0530

    1 file modified

--- src/app/payments/stripe-document/stripe-document.component.ts
+++ src/app/payments/stripe-document/stripe-document.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { StripeService } from "ngx-stripe";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 
 @Component({
-  selector: 'app-stripe-document',
-  templateUrl: './stripe-document.component.html',
-  styleUrls: ['./stripe-document.component.scss']
+  selector: "app-stripe-document",
+  templateUrl: "./stripe-document.component.html",
+  styleUrls: ["./stripe-document.component.scss"]
 })
 export class StripeDocumentComponent implements OnInit {
+  file: any;
+  constructor(private stripeService: StripeService, private http: HttpClient) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit() {}
+  onFileSelect(event) {
+    this.selectedFiles = event.target.files;
+    this.file = event.target.files;
   }
 
+  uploadDocument() {
+    var fd = new FormData();
+    // fd.set('purpose', 'identity_document');
+    // fd.set('file', document.getElementById('file').files[0]);
+    this.debug("Uploading document...");
+    return fetch("https://files.stripe.com/v1/files", {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${stripe._apiKey}`
+      },
+      body: fd
+    })
+      .then(function(r) {
+        return r.json();
+      })
+      .then(function(response) {
+        this.debug(JSON.stringify(response, null, 2));
+        return response.id;
+      });
+  }
+  debug(message) {
+    var debugMessage = document.getElementById("debug-message");
+    console.log("DEBUG: ", message);
+    debugMessage.innerText += "\n" + message;
+  }
 }
