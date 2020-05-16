df103f5d4f4b8ecfe15b7741633c91dc3089da6f
niteshrupesh4 <niteshrupesh4@gmail.com>
2020-05-16 20:55:27 +0530

    2 files modified

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,15 +1,74 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-forms',
-  templateUrl: './forms.component.html',
-  styleUrls: ['./forms.component.css']
+  selector: "app-forms",
+  templateUrl: "./forms.component.html",
+  styleUrls: ["./forms.component.css"]
 })
+// custom validator to check that two fields match
 export class FormsComponent implements OnInit {
-
-  constructor() { }
+  setForm: FormGroup;
+  constructor(private _formBuilder: FormBuilder) {}
 
   ngOnInit() {
+    this.initializationForm();
   }
 
+  /**
+   * get : f
+   * Purpose : returns form controls value
+   * @return form controls
+   */
+  get f() {
+    return this.setForm.controls;
+  }
+
+  /**
+   * Method : initializationForm
+   * Purpose : First time Form initialization
+   */
+  initializationForm() {
+    this.setForm = this._formBuilder.group(
+      {
+        newPassword: ["", [Validators.required]],
+        confirmPassword: ["", [Validators.required]],
+        email: ["", [Validators.required]],
+        confirmEmail: ["", [Validators.required]]
+      },
+      {
+        validator: MustMatch("newPassword", "confirmPassword"),
+        validator1: MustMatch("email", "confirmEmail")
+      }
+    );
+  }
+
+  /**
+   * Method: onSubmit
+   * Purpose : submit form
+   */
+  onSubmit() {
+    if (this.setForm.invalid) {
+      return;
+    } else {
+    }
+  }
+}
+export function MustMatch(controlName: string, matchingControlName: string) {
+  return (formGroup: FormGroup) => {
+    const control = formGroup.controls[controlName];
+    const matchingControl = formGroup.controls[matchingControlName];
+
+    if (matchingControl.errors && !matchingControl.errors.mustMatch) {
+      // return if another validator has already found an error on the matchingControl
+      return;
+    }
+
+    // set error on matchingControl if validation fails
+    if (control.value !== matchingControl.value) {
+      matchingControl.setErrors({ mustMatch: true });
+    } else {
+      matchingControl.setErrors(null);
+    }
+  };
 }
